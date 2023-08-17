c75d62595e3ec84428970b0faaf9221319f248c9
Marmik kumar <marmik.kumar.ece23@heritageit.edu.in>
2023-08-17 15:33:12 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '245840f6a7msh90b9444e5d3b963p1a16c3jsne283804fde10',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
